5437d2229f67f9f0dd28bd0f51b0d277e479be0f
mehrnooshgholami66 <mehrnoosh.gholami.66@gmail.com>
2025-12-17 14:59:24 +0330

    edit authentication object return

--- core/auth.py
+++ core/auth.py
@@ -8,7 +8,7 @@ def authenticate(username, password):
     cursor = conn.cursor()
 
     cursor.execute("""
-        SELECT id, role
+        SELECT id, role, is_active
         FROM users
         WHERE username=? AND password=? AND is_active=1
     """, (username, password))
@@ -16,5 +16,5 @@ def authenticate(username, password):
     user = cursor.fetchone()
     conn.close()
 
-    return user  # None یا (id, role)
+    return user  # None یا (id, role, is_active)
 
