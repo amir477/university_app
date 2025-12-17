8005f7e1babd5a021c63f4a438fb0a87291db054
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2025-12-17 06:11:57 +0100

    create admin action

---
+++ core/user_action.py
@@ -0,0 +1,55 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from core.database import get_connection
+
+def create_user_role(username, password, role):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO users (username, password, role)
+        VALUES (?, ?, ?)
+    """, (username, password, role))
+
+    conn.commit()
+    conn.close()
+
+def block_user(username):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE users
+        SET is_active = 0
+        WHERE username = ?
+    """, (username,))
+
+    conn.commit()
+    conn.close()
+
+def unblock_user(username):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE users
+        SET is_active = 1
+        WHERE username = ?
+    """, (username,))
+
+    conn.commit()
+    conn.close()
+
+def delete_user(username):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM users
+        WHERE username = ?
+    """, (username,))
+
+    conn.commit()
+    conn.close()
